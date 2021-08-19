0bd6a05a32c240534a494a55984561cbd8ab12e6
Sallanez <saul.alejandro19@gmail.com>
2021-08-19 14:21:39 -0600

    Mi primera app para moviles

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+import { blue } from 'ansi-colors';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.paragraph}>Saúl Alejandro Laínez Mejía{"\n"}</Text>
+      <Text style={styles.paragraph}>Hobbies:😜{"\n"}</Text>
+      <Text style={styles.list}>
+        *Leer libros📚{"\n"}
+        *Aprender🔑{"\n"}
+        *Jugar Videojuegos⚔{"\n"}
+        *Cocinar🍽{"\n"}
+      </Text>
+      <Image source={require('./assets/my.png')} style={styles.imageMy}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +24,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  paragraph:{
+    fontSize: 20,
+    color: '#fbff0d',
+  },
+  imageMy:{
+    height: 180,
+    width: 180,
+  },
+  list:{
+    color: '#fff',
   },
 });
